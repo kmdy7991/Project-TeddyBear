740ff62db72eaa10479d9f12bd51989f992bed0d
kmdy7991 <kmdy7991@naver.com>
2024-03-05 19:08:24 +0900

    day 2

---
+++ 이대영/python_example/7_string/main.py
@@ -0,0 +1,8 @@
+text = input('enter text')
+print(len(text))
+
+print(text[0])
+print(text[:])
+
+print(text.find('a'))
+print(text.replace('c', "*"))
---
+++ 이대영/python_example/8_file/main.py
@@ -0,0 +1,2 @@
+with open('test.txt', 'a', encoding='utf-8') as f:
+    f.write('\n파이썬으로 작성한 구문입니다.')
