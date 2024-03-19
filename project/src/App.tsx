70531a3061ca35a7da223e5aed56e9d7dcc41cad
이서윤 <s2yunn@gmail.com>
2024-03-19 16:21:33 +0900

    Feat: 컴포넌트 설계

---
+++ project/src/App.tsx
@@ -0,0 +1,45 @@
+import styles from "./App.module.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+import MyLecture from "./pages/MyPage/MyLecture/MyLecture";
+import Statistics from "./pages/MyPage/Statstics/Statstics";
+import MyNote from "./pages/MyPage/MyNote/MyNote";
+import Search from "./pages/Search/Search";
+import Test from "./pages/Test/Test";
+import VocabularyList from "./pages/VocabularyList/VocabularyList";
+import MyVocabulary from "./pages/VocabularyList/MyVocabulary";
+import ClassVocabulary from "./pages/VocabularyList/ClassVocabulary";
+import VideoDetail from "./pages/Video/VideoDetail";
+import Nav from "./components/Nav";
+function App() {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div className={styles.App}>
+          <Nav className={styles.Nav} />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Main />} />
+            <Route path="mypage" element={<Statistics />}>
+              <Route path="myLecture" element={<MyLecture />} />
+              <Route path="myNote" element={<MyNote />} />
+            </Route>
+            <Route path="search" element={<Search />} />
+            <Route path="test" element={<Test />} />
+            <Route path="vocalist" element={<VocabularyList />}>
+              <Route path="myvoca" element={<MyVocabulary />} />
+              <Route path="classvoca" element={<ClassVocabulary />} />
+            </Route>
+            <Route path="videoDetail" element={<VideoDetail />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
