5df934c02749c97b789a638630bdf434adff4f17
kmdy7991 <kmdy7991@naver.com>
2024-03-19 10:47:45 +0900

    init

--- catalog-service/src/main/java/com/study/catalogservice/service/CatalogService.java
+++
@@ -1,9 +0,0 @@
-package com.study.catalogservice.service;
-
-import com.study.catalogservice.jpa.CatalogEntity;
-
-import java.util.List;
-
-public interface CatalogService {
-    List<CatalogEntity> getAllCatalogs();
-}
--- catalog-service/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into catalog(product_id, product_name, stock, unit_price)
-values ('CATALOG-001', 'Berlin', 100, 1500);
-
-insert into catalog(product_id, product_name, stock, unit_price)
-values ('CATALOG-002', 'Tokyo', 200, 4500);
-
-insert into catalog(product_id, product_name, stock, unit_price)
-values ('CATALOG-003', 'Seoul', 300, 5500);
