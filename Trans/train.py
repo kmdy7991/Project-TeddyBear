d48ff7a540bed6fe89a297915af8b790fdc7c3b5
박지운 <zieunpk0924@naver.com>
2024-03-21 15:52:50 +0900

    Feat: 번역 모델 학습 준비 완료

--- Trans/train.py
+++ Trans/train.py
@@ -1,18 +1,52 @@
-from transformers import Trainer, TrainingArguments
+from transformers import Seq2SeqTrainer
+import numpy as np
 
-training_args = TrainingArguments(
-  output_dir='./results',          
-  num_train_epochs=1,              
-  per_device_train_batch_size=4,  
-  per_device_eval_batch_size=4,   
-  warmup_steps=500,                
-  weight_decay=0.01,               
-  logging_dir='./logs',            
-)
 
-trainer = Trainer(
-  model=model,
-  args=training_args,
-  train_dataset=dataset['train'],
-  eval_dataset=dataset['test']
-)
+# # compute_metrics 함수 정의
+# def compute_metrics(eval_preds):
+#     preds, labels = eval_preds
+#     if isinstance(preds, tuple):
+#         preds = preds[0]
+#     decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+#     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+#     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+#     decoded_preds = [pred.strip() for pred in decoded_preds]
+#     decoded_labels = [[label.strip()] for label in decoded_labels]
+#     result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+#     result = {"bleu": result["score"]}
+#     return result
+
+# # 학습 설정
+# training_args = Seq2SeqTrainingArguments(
+#     output_dir="chkpt",
+#     learning_rate=0.0005,
+#     weight_decay=0.01,
+#     per_device_train_batch_size=64,
+#     per_device_eval_batch_size=128,
+#     num_train_epochs=1,
+#     save_steps=500,
+#     save_total_limit=2,
+#     evaluation_strategy="epoch",
+#     logging_strategy="no",
+#     predict_with_generate=True,
+#     fp16=False,
+#     gradient_accumulation_steps=2,
+#     report_to="none" # Wandb 로그 끄기
+# }
+
+# # Trainer 초기화
+# trainer = Seq2SeqTrainer(
+#     model,
+#     training_args,
+#     train_dataset=tokenized_datasets["train"],
+#     eval_dataset=tokenized_datasets["valid"],
+#     data_collator=data_collator,
+#     tokenizer=tokenizer,
+#     compute_metrics=compute_metrics,
+# )
+
+# # 학습 시작
+# trainer.train()
+
+# # 학습 결과 저장
+# trainer.save_model("./results")
