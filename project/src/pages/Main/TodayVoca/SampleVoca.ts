1e148b0c91fa90e3fe3e6778189c4a10eefc85b7
이서윤 <s2yunn@gmail.com>
2024-03-24 21:11:49 +0900

    Feat: 오늘의 영단어 영상 목록 레이아웃

--- project/src/pages/Main/TodayVoca/SampleVoca.ts
+++ project/src/pages/Main/TodayVoca/SampleVoca.ts
@@ -3,47 +3,211 @@ const SampleVoca = [
     word: "aberration",
     partOfSpeech: "noun",
     meaning: "정상, 평균, 또는 예상에서 벗어난 것, 일반적으로 환영받지 않는",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
   {
     word: "capricious",
     partOfSpeech: "adjective",
     meaning: "기분이나 행동이 갑자기 그리고 예측할 수 없게 변하는",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
-  { word: "debilitate", partOfSpeech: "verb", meaning: "약하게 만들다" },
+  {
+    word: "debilitate",
+    partOfSpeech: "verb",
+    meaning: "약하게 만들다",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
+  },
   {
     word: "euphoria",
     partOfSpeech: "noun",
     meaning: "강렬한 흥분과 행복의 상태 또는 느낌",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
   {
     word: "flabbergasted",
     partOfSpeech: "adjective",
     meaning: "매우 놀란 또는 깜짝 놀란",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
   {
     word: "gregarious",
     partOfSpeech: "adjective",
     meaning: "교우성이 있는; 사교적인",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
   {
     word: "hubris",
     partOfSpeech: "noun",
     meaning: "지나친 자부심 또는 자신감",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
   {
     word: "idiosyncrasy",
     partOfSpeech: "noun",
     meaning: "개인에게 특유한 행동 모드 또는 사고 방식",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
   {
     word: "juxtapose",
     partOfSpeech: "verb",
     meaning: "대조 효과를 위해 가까이 배치하거나 다루다",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
   {
     word: "kudos",
     partOfSpeech: "noun",
     meaning: "달성한 것에 대한 칭찬과 명예",
+    videos: [
+      {
+        id: "1",
+        title: "Understanding Aberration",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/1Rr-pZoftho/maxresdefault.jpg",
+      },
+      {
+        id: "2",
+        title: "Aberration in Science",
+        description:
+          "Explore breathtaking landscapes captured from around the world.",
+        imageUrl: "https://img.youtube.com/vi/uXZVfNJ_EXY/maxresdefault.jpg",
+      },
+    ],
   },
 ];
 
