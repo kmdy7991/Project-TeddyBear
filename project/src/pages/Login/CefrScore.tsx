d8631dc82d90cae709b1c3f1907c32a80639b824
문경림 <mkl9900@naver.com>
2024-03-25 17:52:15 +0900

    Feat:TestModal

--- project/src/pages/Login/CefrScore.tsx
+++ project/src/pages/Login/CefrScore.tsx
@@ -1,39 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import styles from './CefrScore.module.css'
 
-const ScorePage: React.FC = () => {
-  const location = useLocation();
+const CefrScore: React.FC<{ score: number; onClose: () => void; }> = ({ score, onClose }) => {
   const [level, setLevel] = useState<string>('');
 
   useEffect(() => {
-    const { score } = location.state as { score: number };
     const initialLevel = determineLevel(score);
     setLevel(initialLevel);
-  }, [location]);
+  }, [score]);
 
   const determineLevel = (score: number): string => {
     if (score >= 0 && score <= 10) {
-      return 'A0 레벨';
+      return 'A0';
     } else if (score >= 11 && score <= 20) {
-      return 'A1 레벨';
+      return 'A1';
     } else if (score >= 21 && score <= 32) {
-      return 'A2 레벨';
+      return 'A2';
     } else if (score >= 33 && score <= 45) {
-      return 'B1 레벨';
+      return 'B1';
     } else if (score >= 46 && score <= 61) {
-      return 'B2 레벨';
+      return 'B2';
     } else if (score >= 62 && score <= 70) {
-      return 'C1 레벨';
+      return 'C1';
     } else {
       return '점수 범위를 벗어났습니다';
     }
   };
+
   return (
-    <div>
-      <h1>점수로 레벨 확인하기</h1>
-      <p>당신의 레벨은 {level}입니다!</p>
+    <div className={styles.box}>
+      <div className={styles.boxinbox}>
+        <h1 className={styles.inboxtext}>{level}</h1>
+      </div>
+      <h1 className={styles.text}>레벨 테스트 결과</h1>
+      <h1 className={styles.text}>OO님의 레벨은 {level}입니다!</h1>
     </div>
   );
 }
 
-export default ScorePage;
+export default CefrScore;
