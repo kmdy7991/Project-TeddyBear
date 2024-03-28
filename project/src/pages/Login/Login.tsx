f4a64397d2c83d2c7d137e418d136a9fe03a195c
문경림 <mkl9900@naver.com>
2024-03-28 13:05:28 +0900

    Feat:Google Login

--- project/src/pages/Login/Login.tsx
+++ project/src/pages/Login/Login.tsx
@@ -1,15 +1,26 @@
 import yteddy from '../../assets/yellow_teddy.png';
 import styles from './Login.module.css'
+import { useNavigate } from 'react-router-dom';
+import loading from './Loading';
+import { useEffect } from 'react';
 
 
 
 function Login() {
+  const navigate = useNavigate();
+
+  const handleGooglelogin = () => {
+    // window.location.href = `${ process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI }`;
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI}&response_type=code&scope=email+profile`;
+  };
+ 
+
   return (
     <div className={`${styles.pageContainer}`}>
       <div className={`${styles.description}`}>
         <div className={`${styles.title}`}>TEDDYBEAR</div>
         <div>
-          <button className={styles.loginbutton}>구글소셜 로그인</button>
+          <button onClick={handleGooglelogin} className={styles.loginbutton}>구글소셜 로그인</button>
 
         </div>
       </div>
