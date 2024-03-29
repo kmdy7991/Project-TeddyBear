6471b85a240cb02ba89d6436be2d389d3f397f78
westyunn <s2yunn@gmail.com>
2024-03-30 05:10:05 +0900

    Feat: 검색 페이지 api 연결

--- project/src/pages/Search/Search.tsx
+++ project/src/pages/Search/Search.tsx
@@ -1,29 +1,73 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, MouseEvent, useState } from "react";
 import Nav from "../../components/Nav/Nav";
 import styles from "./Search.module.css";
 import SearchInput from "./SearchInput";
 import CategoryDummy from "../../components/Video/CategoryDummy";
 import CategoryBox from "../../components/Video/CategoryBox";
+import { useNavigate } from "react-router-dom";
+
+export interface VideoResultProps {
+  id: number;
+  videoTitle: string;
+  videoDiscription: string;
+  videoUrl: string;
+  videoId: string;
+  videoTime: string;
+  videoThumbnail: string;
+  videoGrade: string;
+}
 function Search() {
   const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState<VideoResultProps[]>([]); // 검색 결과 상태 추가
+
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const nextValue = e.target.value;
     console.log(nextValue);
     setSearchValue(nextValue);
   }
+
   return (
     <div className={`${styles.container}`}>
       <Nav />
       <div className={`${styles.mainContent}`}>
-        <SearchInput value={searchValue} onChange={handleChange} />
-        <div className={`${styles.category}`}>
-          <h2 className={`${styles.title}`}>모두 둘러보기</h2>
-          <div className={`${styles.categoryBox}`}>
-            {CategoryDummy.map((categoryData, index) => (
-              <CategoryBox data={categoryData} key={index} />
+        <SearchInput
+          value={searchValue}
+          onChange={handleChange}
+          onSearch={setSearchResults}
+        />
+        {searchValue.length > 0 ? (
+          <div className={`${styles.videolist}`}>
+            {searchResults?.map((result, index) => (
+              <div
+                className={`${styles.video}`}
+                onClick={() => navigate(`/video/${result.id}`)}
+              >
+                <div className={`${styles.videoImg}`}>
+                  <img src={result.videoThumbnail} alt="썸네일 이미지" />
+                </div>
+                <div className={`${styles.text}`}>
+                  <div className={`${styles.dt}`}>
+                    <div className={`${styles.dif}`}>{result.videoGrade}</div>
+                    <div className={`${styles.title}`}>{result.videoTitle}</div>
+                  </div>
+                  <div className={`${styles.description}`}>
+                    {result.videoDiscription}
+                  </div>
+                </div>
+              </div>
             ))}
           </div>
-        </div>
+        ) : (
+          <div className={`${styles.category}`}>
+            <h2 className={`${styles.title}`}>모두 둘러보기</h2>
+            <div className={`${styles.categoryBox}`}>
+              {CategoryDummy.map((categoryData, index) => (
+                <CategoryBox data={categoryData} key={index} />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- project/src/pages/Search/SearchInput.tsx
+++ project/src/pages/Search/SearchInput.tsx
@@ -1,19 +1,30 @@
-import { ChangeEvent, MouseEvent } from "react";
+import { ChangeEvent, MouseEvent, useState } from "react";
 import styles from "./SearchInput.module.css";
 import search from "../../assets/inputsearch.png";
+import axios from "axios";
 interface SearchInputProps {
   value?: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearch: (results: any[]) => void; // 검색 결과를 설정하는 함수 타입 추가
 }
 
 export default function SearchInput({
   value = "",
   onChange,
+  onSearch,
 }: SearchInputProps) {
-  function handleSearchClick(e: MouseEvent<HTMLButtonElement>) {
+  const handleSearchClick = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log(value);
-  }
+    try {
+      const response = await axios.get(
+        `/video-service/videos/${encodeURIComponent(value)}`
+      );
+      console.log(response);
+      onSearch(response.data); // 검색 결과를 상위 컴포넌트에 전달
+    } catch (err) {
+      console.error("Search request failed", err);
+    }
+  };
   return (
     <div className={`${styles.inputContainer}`}>
       <input
