df2976b594d743989d28907d862484d6f3a86fff
최은희 <gilukji226@naver.com>
2024-04-02 01:05:42 +0900

    Fix: 소셜 로그인 및 연결 페이지 수정

--- project/src/pages/Login/Loading.tsx
+++ project/src/pages/Login/Loading.tsx
@@ -1,9 +1,52 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
+function Loading() {
+  const navigate = useNavigate();
 
-function Loading()  {
-  return (
-    <h1>로딩페이지</h1>
-  );
-};
+  useEffect(() => {
+    console.log("오키");
+    const response = axios.get(`/user-service/fetchId`);
+    response
+      .then((axiosResponse) => {
+        const id = axiosResponse.data; // 데이터
+        const nickname = axios.get(`/user-service/checkNickname/${id}`);
+        console.log("nickname");
+        console.log(nickname);
+        nickname
+          .then((axiosResponse) => {
+            console.log("닉네임");
+            console.log(axiosResponse.data);
+            if (axiosResponse.data) {
+              const response = axios.get(`/user-service/checkTier/${id}`);
+              response
+                .then((axiosResponse) => {
+                  console.log("티어");
+                  console.log(axiosResponse.data);
+                  if (axiosResponse.data) {
+                    navigate(`/`);
+                  } else {
+                    navigate(`/cefrtest`);
+                  }
+                })
+                .catch((error) => {
+                  console.error(error); // 오류 처리
+                });
+            } else {
+              navigate(`/profil`);
+            }
+          })
+          .catch((error) => {
+            console.error(error); // 오류 처리
+          });
+      })
+      .catch((error) => {
+        console.error(error); // 오류 처리
+      });
+  }, []);
+
+  return <h1>로딩페이지</h1>;
+}
 
 export default Loading;
