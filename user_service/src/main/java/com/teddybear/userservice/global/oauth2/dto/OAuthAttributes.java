1c946c4d13589277c7933ba102bd2cacd758d420
최다희 <dahee121816@gmail.com>
2024-04-02 23:07:09 +0900

    Add: userservice

---
+++ user_service/src/main/java/com/teddybear/userservice/global/oauth2/dto/OAuthAttributes.java
@@ -0,0 +1,64 @@
+package com.teddybear.userservice.global.oauth2.dto;
+
+import com.teddybear.userservice.domain.entity.Role;
+import com.teddybear.userservice.domain.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+
+    private Map<String, Object> attributes;
+    private String nameAttributeKey; // OAuth2 로그인 진행 시 키가 되는 필드 값, PK와 같은 의미
+    private String name;
+    private String email;
+    private String picture;
+    private String concern;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes,
+                           String nameAttributeKey, String name,
+                           String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    /**
+     * registrationId는 GOOGLE
+     * 파라미터 : userNameAttributeName -> OAuth2 로그인 시 키(PK)가 되는 값 / attributes : OAuth 서비스의 유저 정보들
+     * 소셜별 of 메소드(ofGoogle, ofKaKao, ofNaver)들은 각각 소셜 로그인 API에서 제공하는
+     * 회원의 식별값(id), attributes, nameAttributeKey를 저장 후 build
+     */
+    public static OAuthAttributes of(String registrationId,
+                                     String userNameAttributeName,
+                                     Map<String, Object> attributes) {
+
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    // 구글 생성자
+    private static OAuthAttributes ofGoogle(String usernameAttributeName,
+                                            Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(usernameAttributeName)
+                .build();
+    }
+
+    // User 엔티티 생성
+    public User toEntity(String email, String concern) {
+        return User.builder()
+                .email(email)
+                .role(Role.GUEST)
+                .concern(concern)
+                .build();
+    }
+}
