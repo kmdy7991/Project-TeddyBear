589dd07dd651b00fc819de22bb6d439e8f2d08fe
박지운 <zieunpk0924@naver.com>
2024-04-03 14:43:29 +0900

    Feat: 학습테스트 문제생성
    Former-commit-id: ae24041e2226c8cae6eadaeff40199e473966826

--- python-final/app/main.py
+++ python-final/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 from app.domain.script_voice.voice_api.voice_main import voice_router
 from app.domain.translate.trans_main import trans_router
+from app.domain.test.test_main import test_router
 import py_eureka_client.eureka_client as eureka_client
 from contextlib import asynccontextmanager
 from starlette.middleware.cors import CORSMiddleware
@@ -30,6 +31,7 @@ app.add_middleware(
 # API Router를 등록합니다.
 app.include_router(voice_router)
 app.include_router(trans_router)
+app.include_router(test_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8779)
