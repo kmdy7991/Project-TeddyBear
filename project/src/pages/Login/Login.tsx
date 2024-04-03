1dc7f5b2ec6352565517c5d3bf4984b88b7f560b
박지운 <zieunpk0924@naver.com>
2024-04-04 01:19:02 +0900

    Update project/src/pages/Login/Login.tsx

--- project/src/pages/Login/Login.tsx
+++ project/src/pages/Login/Login.tsx
@@ -8,7 +8,7 @@ function Login() {
   // 구글 로그인 버튼을 클릭할 때 호출되는 함수
   const handleGoogleLogin = async () => {
     setIsLogin(true);
-    window.location.href = `https://j10b107.p.ssafy.io:8086/oauth2/authorization/google`;
+    window.location.href = `http://j10b107.p.ssafy.io:8086/oauth2/authorization/google`;
   };
 
   return (
