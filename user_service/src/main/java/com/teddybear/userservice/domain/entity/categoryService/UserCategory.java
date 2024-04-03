04518cb4680495f468f87800b211c24fd2b73c4f
박지운 <zieunpk0924@naver.com>
2024-04-03 21:45:04 +0900

    Feat: 인증서 변경

---
+++ user_service/src/main/java/com/teddybear/userservice/domain/entity/categoryService/UserCategory.java
@@ -0,0 +1,43 @@
+package com.teddybear.userservice.domain.entity.categoryService;
+
+import com.teddybear.userservice.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "user_category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_category_seq")
+    private Long userCategorySeq;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_seq")
+    private User user;
+
+    @Column(columnDefinition = "bigint default 0")
+    private Long life;
+
+    @Column(columnDefinition = "bigint default 0")
+    private Long society;
+
+    @Column(columnDefinition = "bigint default 0")
+    private Long it;
+
+    @Column(columnDefinition = "bigint default 0")
+    private Long sports;
+
+    @Column(columnDefinition = "bigint default 0")
+    private Long world;
+
+    @Column(columnDefinition = "bigint default 0")
+    private Long politics;
+
+    @Column(columnDefinition = "bigint default 0")
+    private Long economy;
+}
