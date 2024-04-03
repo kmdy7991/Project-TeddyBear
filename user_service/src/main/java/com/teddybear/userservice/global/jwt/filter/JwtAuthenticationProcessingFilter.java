f269e3eefcad57bf67b4f3dc37696bc2060680c7
최은희 <gilukji226@naver.com>
2024-04-03 17:56:36 +0900

    Fix: log uri

--- user_service/src/main/java/com/teddybear/userservice/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ user_service/src/main/java/com/teddybear/userservice/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -48,6 +48,7 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
             return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
         }
+        log.info("uri = {}", request.getRequestURI());
 
         // 사용자 요청 헤더에서 RefreshToken 추출
         // -> RefreshToken이 없거나 유효하지 않다면(DB에 저장된 RefreshToken과 다르다면) null을 반환
