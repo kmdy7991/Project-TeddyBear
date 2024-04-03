caf9d876231d0bccc13dd099e08042d31b2ad784
kmdy7991 <kmdy7991@naver.com>
2024-04-04 01:12:41 +0900

    init

--- category_service/src/main/java/com/teddybear/categoryservice/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.teddybear.categoryservice.service;
-
-import com.teddybear.categoryservice.response.CategoryResponseDto;
-
-import java.util.List;
-
-public interface CategoryService {
-    List<CategoryResponseDto> getCategorys();
-}
