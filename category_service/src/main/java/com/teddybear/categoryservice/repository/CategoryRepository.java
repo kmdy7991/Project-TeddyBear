b10427c50b11e3c5ba1170dea7c32e75d6ca524f
최다희 <dahee121816@gmail.com>
2024-04-03 16:17:34 +0900

    Feat: child branch

--- category_service/src/main/java/com/teddybear/categoryservice/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teddybear.categoryservice.repository;
-
-import com.teddybear.categoryservice.entity.VideoCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<VideoCategory, Long> {
-}
--- video_service/src/main/java/com/teddybear/videoservice/jpa/TranslatedVideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.teddybear.videoservice.jpa;
-
-import com.teddybear.videoservice.vo.VideoDto;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TranslatedVideoRepository extends MongoRepository<VideoDto, String> {
-}
