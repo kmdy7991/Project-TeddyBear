7944fa1b0a2fe67c4d9c2a70e4e98541214032c1
최은희 <gilukji226@naver.com>
2024-04-03 09:31:04 +0900

    Feat: 카테고리별 영상 리스트 조회 기능

--- category_service/src/main/java/com/teddybear/categoryservice/repository/CategoryRepository.java
+++ category_service/src/main/java/com/teddybear/categoryservice/repository/CategoryRepository.java
@@ -2,6 +2,12 @@ package com.teddybear.categoryservice.repository;
 
 import com.teddybear.categoryservice.entity.VideoCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<VideoCategory, Long> {
+    @Query("SELECT videoId From VideoCategory WHERE category = :category")
+    List<String> findVideoIdByCategory(@Param("category") String category);
 }
