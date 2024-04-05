c210cdf9ff226a0b03d62aae226c33b1ae4d559c
문경림 <mkl9900@naver.com>
2024-04-05 10:39:15 +0900

    update: test

--- project/src/pages/Login/Login.tsx
+++ project/src/pages/Login/Login.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import yteddy from "../../assets/yellow_teddy.png";
 import styles from "./Login.module.css";
+import glogin from "../../assets/googlelogin.png";
 
 function Login() {
   const [isLogin, setIsLogin] = useState(false);
 
-  // 구글 로그인 버튼을 클릭할 때 호출되는 함수
+  // 구글 로그인 처리를 위한 함수
   const handleGoogleLogin = async () => {
     setIsLogin(true);
     window.location.href = `http://j10b107.p.ssafy.io:8086/oauth2/authorization/google`;
@@ -15,8 +16,9 @@ function Login() {
     <div className={`${styles.pageContainer}`}>
       <div className={`${styles.description}`}>
         <div className={`${styles.title}`}>TEDDYBEAR</div>
+        {/* 이미지에 onClick 이벤트 핸들러 추가 */}
         <div onClick={handleGoogleLogin} className={styles.loginbutton}>
-          <button>구글 소셜 로그인</button>
+          <img src={glogin} alt="구글 로그인" className={styles.googleLoginImage} />
         </div>
       </div>
       <div className={`${styles.loginimg}`}>
