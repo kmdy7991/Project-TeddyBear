6987e7fb4c38dd316d8d6c6ebf73eff13d8281fb
최다희 <1218dahee@naver.com>
2024-04-09 09:29:28 +0900

    Docs: add db dump

---
+++ exec/TeddyBear_dumps/test_user_category.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: j10b107.p.ssafy.io    Database: test
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_category`
+--
+
+DROP TABLE IF EXISTS `user_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_category` (
+  `user_category_seq` bigint NOT NULL AUTO_INCREMENT,
+  `economy` bigint DEFAULT '0',
+  `it` bigint DEFAULT '0',
+  `life` bigint DEFAULT '0',
+  `politics` bigint DEFAULT '0',
+  `society` bigint DEFAULT '0',
+  `sports` bigint DEFAULT '0',
+  `world` bigint DEFAULT '0',
+  `user_seq` bigint DEFAULT NULL,
+  PRIMARY KEY (`user_category_seq`),
+  UNIQUE KEY `UK_ssgf22pmvpx3mmxqtfb3enlu3` (`user_seq`),
+  CONSTRAINT `FK454v6vknodr6807kw80i0yxqe` FOREIGN KEY (`user_seq`) REFERENCES `users` (`user_seq`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_category`
+--
+
+LOCK TABLES `user_category` WRITE;
+/*!40000 ALTER TABLE `user_category` DISABLE KEYS */;
+INSERT INTO `user_category` VALUES (1,10,10,10,10,10,10,10,2),(2,10,10,10,11,10,10,10,1);
+/*!40000 ALTER TABLE `user_category` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-08 16:01:24
