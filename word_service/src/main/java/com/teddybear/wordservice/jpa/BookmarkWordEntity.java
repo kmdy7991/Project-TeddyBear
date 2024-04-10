5009c5b2bd627f07b28a3a68143b3b60d19b8892
최다희 <dahee121816@gmail.com>
2024-04-10 18:13:49 +0900

    Fix: WordEntity FetchType LAZY

--- word_service/src/main/java/com/teddybear/wordservice/jpa/BookmarkWordEntity.java
+++ word_service/src/main/java/com/teddybear/wordservice/jpa/BookmarkWordEntity.java
@@ -14,7 +14,6 @@ public class BookmarkWordEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//   @ManyToOne(fetch = FetchType.EAGER)
     @ManyToOne
     @JoinColumn(name = "word", nullable = false)
     private WordEntity word;
