14c6e3ed14b4476171c8365561bf3c5fef34919b
최다희 <dahee121816@gmail.com>
2024-04-10 18:32:01 +0900

    Fix: to responseWord

--- word_service/src/main/java/com/teddybear/wordservice/service/WordServiceImpl.java
+++ word_service/src/main/java/com/teddybear/wordservice/service/WordServiceImpl.java
@@ -57,18 +57,41 @@ public class WordServiceImpl implements WordService{
                 .build();
     }
 
+//    @Override
+//    public List<WordEntity> getBookmarkWordsBy(Long userId) {
+//        List<BookmarkWordEntity> bookmarkWordEntities = bookmarkWordRepository.findByUserId(userId);
+//        if (!bookmarkWordEntities.isEmpty()) {
+//            List<Long> wordIds = bookmarkWordEntities.stream()
+//                    .map(bookmarkWordEntity -> bookmarkWordEntity.getWord().getId())
+//                    .collect(Collectors.toList());
+//            return wordRepository.findAllByIdIn(wordIds);
+//        } else {
+//            return new ArrayList<>(); // 유저에 해당하는 북마크된 단어가 없을 경우 빈 리스트를 반환
+//        }
+//    }
     @Override
-    public List<WordEntity> getBookmarkWordsBy(Long userId) {
-        List<BookmarkWordEntity> bookmarkWordEntities = bookmarkWordRepository.findByUserId(userId);
-        if (!bookmarkWordEntities.isEmpty()) {
-            List<Long> wordIds = bookmarkWordEntities.stream()
-                    .map(bookmarkWordEntity -> bookmarkWordEntity.getWord().getId())
-                    .collect(Collectors.toList());
-            return wordRepository.findAllByIdIn(wordIds);
-        } else {
-            return new ArrayList<>(); // 유저에 해당하는 북마크된 단어가 없을 경우 빈 리스트를 반환
-        }
+    public List<ResponseWord> getBookmarkWordsBy(Long userId) {
+        List<BookmarkWordEntity> bookmarkedWords = bookmarkWordRepository.findByUserId(userId);
+        return bookmarkedWords.stream()
+                .map(bookmarkWordEntity -> mapToResponseWord(bookmarkWordEntity.getWord()))
+                .collect(Collectors.toList());
     }
+
+    private ResponseWord mapToResponseWord(WordEntity wordEntity) {
+        return ResponseWord.builder()
+                .id(wordEntity.getId())
+                .eng(wordEntity.getEng())
+                .kor(wordEntity.getKor())
+                .part(wordEntity.getPart())
+                .tier(wordEntity.getTier())
+                .build();
+    }
+
+
+
+
+
+
 
 
 
