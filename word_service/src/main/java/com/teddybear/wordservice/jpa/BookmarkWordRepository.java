3b21619ba336137289e4b613e6d992bcac119c1b
최다희 <dahee121816@gmail.com>
2024-04-11 21:05:36 +0900

    Fix: test2

--- word_service/src/main/java/com/teddybear/wordservice/jpa/BookmarkWordRepository.java
+++ word_service/src/main/java/com/teddybear/wordservice/jpa/BookmarkWordRepository.java
@@ -2,6 +2,8 @@ package com.teddybear.wordservice.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
